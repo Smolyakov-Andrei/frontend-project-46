ccf9eb3172a9236ddbd147f120f0e4250f2f6d91
Smolyakov-Andrei <Chrisztw@mail.ru>
2025-12-05 14:39:18 +0700

    create project

---
+++ bin/gendiff.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+import { Command } from "commander";
+import genDiff from "../src/genDiff.js";
+
+const program = new Command();
+
+program
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .version("1.0.0")
+  .argument("<filepath1>", "first file to compare")
+  .argument("<filepath2>", "second file to compare")
+  .option("-f, --format <type>", "output format", "stylish")
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
+  })
+  .parse();
